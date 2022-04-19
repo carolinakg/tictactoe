66dcb85689e3ec816fd38d8f8c3be9ba83819fcd
carolinakg <carol.ghelman@hotmail.com>
2022-04-19 17:27:19 +0300

    initial

---
+++ tictactoe/src/App.js
@@ -0,0 +1,16 @@
+import {Route, Routes} from "react-router-dom";
+import './App.css';
+import Home from "./pages/Home";
+import Game from "./pages/Game";
+
+function App() {
+  return (
+    <Routes>
+      <Route exact path="/" element={<Home/>}/>
+      <Route exact path="/game" element={<Game/>}/>
+  
+    </Routes>
+  );
+}
+
+export default App;
